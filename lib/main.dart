a08a508e772b39101382971c909514092f0ff642
sridharvs <sridharvsvs@gmail.com>
2023-12-18 13:17:46 +0530

    firebase project created

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_wallmsg_auth_app/auth/login_or_register.dart';
+import 'package:flutter_wallmsg_auth_app/firebase_options.dart';
 import 'package:flutter_wallmsg_auth_app/pages/login_page.dart';
 import 'package:flutter_wallmsg_auth_app/pages/register_page.dart';
 import 'package:flutter_wallmsg_auth_app/theme/dark_mode.dart';
 import 'package:flutter_wallmsg_auth_app/theme/light_mode.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
